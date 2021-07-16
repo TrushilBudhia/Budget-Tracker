3c6aa54b145029ef8f49c0aacfb8ea60f55d16f4
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-16 19:57:04 +0800

    Added service-worker.js and the dist folder files, pdated package.json and webpack.comfig.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,11 @@ const config = {
       icons: [{
         src: path.resolve("public/icons/icon-192x192.png"),
         sizes: [192, 512],
-        destination: path.join("icons")
+        destination: path.join("assets", "icons")
       }]
     })
   ],
+  // Adding module - babel-loader
   module: {
     rules: [
       {
