82ce1521947a4865a64f0ff3b4a3ae6c4932c6d4
Trushil Budhia <trushil.budhia@gmail.com>
2021-07-16 19:41:57 +0800

    Updated webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "development",
+
+  // Adding entry point - index.js
+  entry: "./public/index.js",
+  // Adding the output directory and filename
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js",
+  },
+  // Adding plugins - WebpackPwaManifest
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget Tracker App",
+      short_name: "Budget Tracker",
+      description: "An application that allows a user to add expenses and deposits to their budget with or without a connection",
+      background_color: "#1d9186",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("public/icons/icon-192x192.png"),
+        sizes: [192, 512],
+        destination: path.join("icons")
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
